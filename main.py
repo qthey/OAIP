1df511977d379a38320098775166cc9f8454ef28
qthey <109228517+qthey@users.noreply.github.com>
2024-09-26 13:33:02 +0900

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-#name_film = input()
-#name_cinema = input()
-#time = input()
-#print('Билеты на "', name_film, '"', 'в', '"', name_cinema, '"', 'на', time, 'забронирован.')
-
-monthly_salary = int(input('Зарплата за месяц:'))
-number_of_hours_worked_on_weekends = int(input('Количество отработанных в выходных часов:'))
-prize = monthly_salary * 0.01 * number_of_hours_worked_on_weekends
-print('Размер премии:', prize)
