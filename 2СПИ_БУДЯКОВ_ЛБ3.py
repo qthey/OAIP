b0fc7a0bdbba06734be0fa5e6d595339c0f56a1f
qthey <109228517+qthey@users.noreply.github.com>
2024-11-20 17:16:31 +0900

    Update 2СПИ_БУДЯКОВ_ЛБ3.py

--- 2СПИ_БУДЯКОВ_ЛБ3.py
+++ 2СПИ_БУДЯКОВ_ЛБ3.py
@@ -1,43 +1,121 @@
-name_film = input()
-name_cinema = input()
-time = input()
-print(f'Билет на " {name_film} " в " {name_cinema} " на {time} забронирован.')
+def main():
 
-monthly_salary = int(input('Зарплата за месяц:'))
-number_of_hours_worked_on_weekends = int(input('Количество отработанных в выходных часов:'))
-prize = monthly_salary * 0.01 * number_of_hours_worked_on_weekends
-print('Размер премии:', prize)
+    # # 1 задание
 
-sum = int(input('Введите сумму:'))
-banknote_1000 = sum // 1000
-sum1 = sum - (1000 * banknote_1000)
-banknote_100 = sum1 // 100
-sum2 = sum1 - (100 * banknote_100)
-coin_10 = sum2 // 10
-sum3 = sum2 - (10 * coin_10)
-coin_1 = sum3 // 1
-print(coin_1, '- по 1р')
-print(coin_10, '- по 10р')
-print(banknote_100, '- по 100р')
-print(banknote_1000, '- по 1000р')
+    # while True:
+    #     x = input("Введите строку:")
+    #     if x == "":
+    #         break
+    #     print("Длина этой строки:", len(x))
 
-comment = input('Оцените развлекательный комплекс:')
-print(comment.find('весело'))
-print(comment.find('увлекательно'))
-print(comment.find('развлечения'))
+    # # 2 задание
 
-word = input()
-print(word[(len(word)-1)//2])
+    # count = 0
+    # while True:
+    #     x = float(input("Введите число:"))
+    #     if x > 36.6:
+    #         break
+    #     elif x <= 36.6:
+    #         count += 1
+    # print("Количество отрицательных чиел:", count)
 
-feedback = 'Алиса и Вася, большое спасибо за теплый приём!'
-name1 = feedback[0:5]
-name2 = feedback[8:12]
-print("Назначить премию:", name1 + '/' + name2)
+    # # 3 задание
 
-numb = int(input())
-alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-if numb + 3 > len(alphabet):
-alphabet = alphabet * 2
-print(alphabet[numb - 1:numb + 3])
-else:
-print(alphabet[numb - 1:numb + 3])
+    # first_max = 0
+    # second_max = 0
+    # while True:
+    #     num = int(input("Введите число: "))
+    #     if abd(num) >= 1000:
+    #         break
+    #     if
+
+    # # 4 задание
+
+    # num = input("Введите числа через пробел: ")
+    # num_list = num.split(" ")
+    # num_list = list(map(float, num_list))
+    # min_num = num_list[0]
+    # counter = 1
+    # while counter < len(num_list):
+    #     if num_list[counter] < min_num:
+    #         min_num = num_list[counter]
+    #     counter += 1
+    # print(min_num)
+
+    # # 5 задание
+
+    # num = float(input("Введите число: "))
+    # while True:
+    #     if num == 0:
+    #         break
+    #     elif num % 3 == 0 and num % 7 == 0:
+    #         print("Караул!")
+    #         break
+    #     elif num % 3 == 0 and num % 7 != 0:
+    #         print("Несчастливое")
+    #     elif num % 7 == 0 and num % 3 != 0:
+    #         print("Опасное")
+    #     else:
+    #         print(num)
+    #     num = float(input("Введите число: "))
+
+    # # 6 задание
+
+    # a = 0
+    # num = 1
+    # while num <= 10000:
+    #     if num % num == 0 and num % 1 == 0:
+    #         a += num
+    #         num += 1
+    #     else:
+    #         break
+    # print(a)
+
+    # # 7 задание
+
+    # # 8 задание
+
+    # word = [input("Введите слово: ")]
+    # small_word = [word[0]]
+    # while word[-1] != "стоп":
+    #     if len(word[-1]) < len(small_word[-1]):
+    #         small_word = [word[-1]]
+    #     word.append(input("Введите слово: "))
+    # print(small_word[0])
+
+    # # 9 задание
+
+    # num = float(input("Введите число: "))
+    # operator = input("Введите операцию: ")
+    # result = num
+    # while operator != "стоп":
+    #     num = float(input("Введите число: "))
+    #     if operator == "+":
+    #         result += num
+    #     elif operator == "-":
+    #         result -= num
+    #     elif operator == "*":
+    #         result *= num
+    #     elif operator == "/":
+    #         result /= num
+    #     elif operator == "**":
+    #         result **= num
+    #     print(result)
+    #     operator = input("Введите операцию: ")
+
+    # # 10 задание
+
+    # result = ""
+    # while True:
+    #     word = input("Введите слово или !: ")
+    #     if word == "стоп":
+    #         break
+    #     result += word + " "
+    #     if word == "!":
+    #         result = result[:-3] + "!"
+    #         result += "\n"
+    # print(result)
+
+
+if __name__ == "__main__":
+    main()
